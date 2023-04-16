9df406314af39330c81741cff5bb1cc49adc80da
mdfaysal <faysal7830@gmail.com>
2023-04-17 02:03:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstNav from './Components/FirstNav/FirstNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-indigo-600 w-full h-screen">
+      <FirstNav></FirstNav>
     </div>
   );
 }
