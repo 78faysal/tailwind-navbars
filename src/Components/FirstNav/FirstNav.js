3d8e4138a27b970d0ff53d4f1252758ff25dbad9
mdfaysal <faysal7830@gmail.com>
2023-04-19 23:22:55 +0600

    nav icon done

--- src/Components/FirstNav/FirstNav.js
+++ src/Components/FirstNav/FirstNav.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FirstNav = () => {
+
+    const [open, setOpen] = useState(false);
+
     return (
 
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                        </li>
-                        <li>
-                            <a href="" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                        </li>
-                    </ul>
+        <nav class="bg-white p-5 shadow ">
+            <div className='container md:flex md:items-center md:justify-between mx-auto'>
+                <div class="">
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        <img className='h-12 inline' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_YWalTprJhGm29nO1N2aHyq8yrwZEHLQXMEoGpD-HAv4PZPqT4Ewl8J0jXAhKBhzv3rw&usqp=CAU" alt="" />
+                        Tailwind Navs</span>
+
                 </div>
+
+                <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-7 cursor-pointer md:hidden'>
+                    <ion-icon name={open ? 'close-outline' : 'menu-outline'}></ion-icon>
+                </div>
+
+                <ul className='md:flex md:items-center md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-3 md:pl-0 pl-5'>
+                    <li className='mx-3 my-4 md:my-0 '>
+                        <a href="" className='text-xl hover:text-indigo-500 duration-500'>Home</a>
+                    </li>
+                    <li className='mx-3 my-4 md:my-0'>
+                        <a href="" className='text-xl hover:text-indigo-500 duration-500'>About</a>
+                    </li>
+                    <li className='mx-3 my-4 md:my-0'>
+                        <a href="" className='text-xl hover:text-indigo-500 duration-500'>Services</a>
+                    </li>
+                    <li className='mx-3 my-4 md:my-0'>
+                        <a href="" className='text-xl hover:text-indigo-500 duration-500'>Contact</a>
+                    </li>
+                    <li className='mx-3 my-4 md:my-0'>
+                        <a href="" className='text-xl hover:text-indigo-500 duration-500'>Blogs</a>
+                    </li>
+                </ul>
             </div>
 
         </nav>
